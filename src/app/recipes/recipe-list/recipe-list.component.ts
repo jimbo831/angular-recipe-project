88facd23884e4a907f71b2307f1baf677787837a
jimbo831 <jimbo831@gmail.com>
2018-05-12 13:59:43 -0500

    Started recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.maxpixel.net/static/photo/1x/' +
+      'Cooking-Meat-Recipe-Dinner-Restaurant-Grill-Beef-2508859.jpg')
+  ];
 
   constructor() { }
 
